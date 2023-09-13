5916843f0d8c24f72100cfb9b6cf2bab27afabcc
dangakeri <dangakeri254@gmail.com>
2023-09-13 09:13:13 +0300

    constructor function

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
+    // Instance properties
     this.firstName = firstName;
     this.birthYear = birthYear;
 }
@@ -12,3 +13,9 @@ console.log(daniel);
 // 2.function is called, this = {}
 // 3.{ } linked to prototype
 // 4.function Automatically returns { }
+
+const matilda = new Person('matilda', 2017);
+const jack = new Person('jack', 2003);
+console.log(matilda, jack);
+
+console.log(daniel instanceof Person);
