adffbfda10f9f20afd774a3225d17588c96f316c
dangakeri <dangakeri254@gmail.com>
2023-09-13 10:41:41 +0300

    prototype

--- starter/script.js
+++ starter/script.js
@@ -19,3 +19,44 @@ const jack = new Person('jack', 2003);
 console.log(matilda, jack);
 
 console.log(daniel instanceof Person);
+
+// Prototype
+console.log(Person.prototype);
+Person.prototype.calcAge = function () {
+    console.log(2037 - this.birthYear);
+}
+daniel.calcAge();
+
+// console.log(daniel.__proto__);
+// console.log(daniel.__proto__ === Person.prototype);
+// /*
+// console.log(Person.prototype.isPrototypeOf(daniel));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
+// */
+// Person.prototype.species = 'Homo Sapiens';
+/*
+console.log(daniel.species, matilda.species);
+
+console.log(daniel.hasOwnProperty('firstName'));
+console.log(daniel.hasOwnProperty('species'));
+
+console.log(daniel.__proto__);
+console.log(daniel.__proto__.__proto__);
+console.log(daniel.__proto__.__proto__.__proto__);
+*/
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+    return [...new Set(this)];
+}
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.log(h1);
